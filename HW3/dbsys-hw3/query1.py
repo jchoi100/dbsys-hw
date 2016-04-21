6a93b6d2e2d07c2021bde2434e7b9959fa3ce85d
Joon Hyuck Choi <joonhyuck.choi92@gmail.com>
2016-04-21 19:09:44 -0400

    exp: q1, q2 pushdown run time check

--- HW3/dbsys-hw3/query1.py
+++ HW3/dbsys-hw3/query1.py
@@ -35,22 +35,22 @@ Un-Optimized
 # print("Un-Optimized Explain: ")
 # print(query1.explain())
 # print("Un-Optimized Results: ")
-qresults = [query1.schema().unpack(tup) \
-       for page in db.processQuery(query1) \
-       for tup in page[1]]
+# qresults = [query1.schema().unpack(tup) \
+#        for page in db.processQuery(query1) \
+#        for tup in page[1]]
 # print(qresults)
 
 """
 Pushdown Option
 """
-# optimized_query = db.optimizer.pushdownOperators(query1)
+optimized_query = db.optimizer.pushdownOperators(query1)
 # print("\n")
 # print("Pushdown Explain: ")
 # print(optimized_query.explain())
 # print("Pushdown Results: ")
-# opt_qresults = [optimized_query.schema().unpack(tup) \
-#         for page in db.processQuery(optimized_query) \
-#         for tup in page[1]]
+opt_qresults = [optimized_query.schema().unpack(tup) \
+        for page in db.processQuery(optimized_query) \
+        for tup in page[1]]
 # print(opt_qresults)
 
 """
